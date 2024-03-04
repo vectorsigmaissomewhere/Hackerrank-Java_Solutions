cd23d04b530cc53bf8a40d052d5e84622d39f47e
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-03-04 21:18:29 +0545

    Update JavaStringsIntroduction.java

--- Strings/JavaStringsIntroduction.java
+++ Strings/JavaStringsIntroduction.java
@@ -9,13 +9,12 @@ public class Solution {
         String A=sc.next();
         String B=sc.next();
         /* Enter your code here. Print output to STDOUT. */
-        String C=A.concat(B);
-        System.out.println(C.length());
-        if(A.length()>B.length()){
-            System.out.println("No");
-        }
-        else if(A.length()<B.length()){
+        System.out.println(A.length()+B.length());
+        if(A.compareTo(B)>0){
             System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
         }
         System.out.println(A.substring(0,1).toUpperCase()
         +A.substring(1)+" "+B.substring(0,1).toUpperCase()
