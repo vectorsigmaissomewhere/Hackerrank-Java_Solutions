e1c009fffee0607373cc5d54ce8c2e963353fbf2
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-05-12 16:42:03 +0545

    Create Java1DArray.java

---
+++ Data Structures/Java1DArray.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++){
+            int b = scan.nextInt();
+            a[i] = b;
+        }
+
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
+
+
+/*
+input
+5
+10
+20
+30
+40
+50
+
+output
+10
+20
+30
+40
+50
+*/
